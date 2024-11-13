7560e69d832fe6ef26d16dcd680e01413d33c73c
deswaq1220 <deswaq789@naver.com>
2024-11-14 01:36:45 +0900

    :lipstick: 코드 포맷팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 const queryClient = new QueryClient();
 
 async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
-    return
+    return;
   }
 
-  const { worker } = await import('./mocks/browser')
+  const { worker } = await import('./mocks/browser');
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start()
+  return worker.start();
 }
 
 enableMocking().then(() => {
@@ -26,5 +25,5 @@ enableMocking().then(() => {
         <App />
       </StrictMode>
     </QueryClientProvider>
-  )
-})
+  );
+});
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from "react-router";
-import DefaultLayout from "../layouts/defaultLayout";
+import { Route, Routes } from 'react-router';
+import DefaultLayout from '../layouts/defaultLayout';
 
 const Router = () => {
   return (
     <>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path="*" />
+          <Route path='*' />
         </Route>
       </Routes>
     </>
   );
-}
-export default Router
+};
+export default Router;
