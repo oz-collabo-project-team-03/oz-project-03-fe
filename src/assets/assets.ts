b27f07b895254f5e6e351fe45d9fceba638edf14
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-14 14:07:26 +0900

    🍱 프로필 수정 아이콘 추가
    #7

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -11,6 +11,8 @@ import userActiveIcon from './navBar/teacher/user_Active.svg';
 import googleLogo from './login/google_login.svg';
 import kakaoLogo from './login/kakao_logo.png';
 import naverLogo from './login/naver_logo.svg';
+
+import editIcon from './myPage/editIcon.svg';
 export {
   backIcon,
   chatActiveIcon,
@@ -21,4 +23,5 @@ export {
   naverLogo,
   plusActiveIcon,
   userActiveIcon,
+  editIcon,
 };
