2fb2be982a005e4fffacf16c77c9b1e9c3c84696
deswaq1220 <deswaq789@naver.com>
2024-11-15 01:47:02 +0900

    :recycle: 높이값 svh로 수정

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 const DefaultLayout = () => {
   return (
     <div className='flex items-center justify-center bg-slate-50 font-pre'>
-      <div className='h-screen w-full bg-white md:w-[425px] lg:w-[425px]'>
+      <div className='h-svh w-full bg-white md:w-[425px] lg:w-[425px]'>
         <Outlet />
       </div>
     </div>
