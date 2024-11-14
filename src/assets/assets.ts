6ff14af5aaf0284f5ede009849f61cfcd5b1e4ae
Yi-HyeonJu <guswwn1925@gmail.com>
2024-11-14 14:12:14 +0900

    🍱 학생 / 선생 프로필 수정 이미지 추가
    #7

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -13,6 +13,24 @@ import kakaoLogo from './login/kakao_logo.png';
 import naverLogo from './login/naver_logo.svg';
 
 import editIcon from './myPage/editIcon.svg';
+
+import stDefaultIcon from './profileEdit/student/stDefaultIcon.svg';
+import stIcon2 from './profileEdit/student/stIcon2.svg';
+import stIcon3 from './profileEdit/student/stIcon3.svg';
+import stIcon4 from './profileEdit/student/stIcon4.svg';
+import stIcon5 from './profileEdit/student/stIcon5.svg';
+import stIcon6 from './profileEdit/student/stIcon6.svg';
+import stIcon7 from './profileEdit/student/stIcon7.svg';
+import stIcon8 from './profileEdit/student/stIcon8.svg';
+import stIcon9 from './profileEdit/student/stIcon9.svg';
+import stIcon10 from './profileEdit/student/stIcon10.svg';
+import stIcon11 from './profileEdit/student/stIcon11.svg';
+import stIcon12 from './profileEdit/student/stIcon12.svg';
+
+import teDefaultIcon from './profileEdit/teacher/teDefaultIcon.svg';
+import teIcon2 from './profileEdit/teacher/teIcon2.svg';
+import teIcon3 from './profileEdit/teacher/teIcon3.svg';
+
 export {
   backIcon,
   chatActiveIcon,
@@ -24,4 +42,19 @@ export {
   plusActiveIcon,
   userActiveIcon,
   editIcon,
+  stDefaultIcon,
+  stIcon2,
+  stIcon3,
+  stIcon4,
+  stIcon5,
+  stIcon6,
+  stIcon7,
+  stIcon8,
+  stIcon9,
+  stIcon10,
+  stIcon11,
+  stIcon12,
+  teDefaultIcon,
+  teIcon2,
+  teIcon3,
 };
